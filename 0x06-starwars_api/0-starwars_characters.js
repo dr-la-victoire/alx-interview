6c2c1f4466f5d5a899606a87a6d2ee12fdf6ab4f
Victory Amadi <lavictoire633@gmail.com>
2024-08-07 15:10:31 +0100

    just checking

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,56 @@
 #!/usr/bin/node
-const request = require('request');
+const { rejects } = require("assert");
+const request = require("request");
 
 const id = process.argv[2];
 
-request(`https://swapi-api.alx-tools.com/api/films/${id}`, function (error, response, body) {
-  if (error) {
-    console.error('Error: ', error);
-  } else if (response.statusCode === 200) {
+function getFilmData(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode === 200) {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
+}
+
+function getCharacterNames(url) {
+  return getFilmData(url).then((data) => data.name);
+}
+
+getFilmData(`https://swapi-api.alx-tools.com/api/films/${id}`)
+  .then((data) => {
+    const characters = data.characters;
+    return Promise.all(characters.map(getCharacterNames));
+  })
+  .then((characterName) => {
+    characterName.forEach((name) => console.log(name));
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
+/*request(
+  `https://swapi-api.alx-tools.com/api/films/${id}`,
+  function (error, response, body) {
+    if (error) {
+      console.error("Error: ", error);
+    } else if (response.statusCode === 200) {
       const data = JSON.parse(body);
       const characters = data.characters;
       characters.forEach((character) => {
-        request(character, function(error, response, body) {
+        request(character, function (error, response, body) {
           if (error) {
-            console.error('Error:', error);
-	  } else if (response.statusCode === 200) {
-              const theData = JSON.parse(body);
-	      const characterName = theData.name;
-	      console.log(characterName);
-	  }
-	});
+            console.error("Error:", error);
+          } else if (response.statusCode === 200) {
+            const theData = JSON.parse(body);
+            const characterName = theData.name;
+            console.log(characterName);
+          }
+        });
       });
+    }
   }
-});
+);*/
