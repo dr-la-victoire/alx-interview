5a0c02408571c1017862ece97077230d8400b750
La Victoire <lavictoire633@gmail.com>
2024-10-13 18:05:44 +0100

    fixing pycodestyle styling errors

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This module solves the change problem with greedy algorithms"""
 
+
 def makeChange(coins, total):
     """Returns the fewest number of coins needed to meet total"""
     # Initialization of the list
     change = [total + 1] * (total + 1)
-    change[0] = 0 # base case
+    change[0] = 0  # base case
 
     # Main loop
     for a in range(1, total + 1):
@@ -16,3 +17,4 @@ def makeChange(coins, total):
     if change[total] != (total + 1):
         return change[total]
     return -1  
+    
