e9f2d0ec06b173d7d5b56cf4521f085d059cae6f
La Victoire <lavictoire633@gmail.com>
2024-10-13 17:40:13 +0100

    solving the change problem with greedy algorithms and dynamic programming

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module solves the change problem with greedy algorithms"""
+
+def makeChange(coins, total):
+    """Returns the fewest number of coins needed to meet total"""
+    # Initialization of the list
+    change = [total + 1] * (total + 1)
+    change[0] = 0 # base case
+
+    # Main loop
+    for a in range(1, total + 1):
+        for coin in coins:
+            if a - coin >= 0:
+                change[a] = min(change[a], 1 + change[a - coin])
+
+    if change[total] != (total + 1):
+        return change[total]
+    return -1  
